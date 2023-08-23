6e548d1b1c5567dbea37ccc9b262ddb602c516cc
Lucas <59517854+lucaswydx@users.noreply.github.com>
2023-08-23 22:53:01 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -4,14 +4,7 @@ Config = {
     respawnTime = 10, -- Change this to the desired respawn time in seconds
     respawnText = "You are dead, press ~c~[E]~w~ to respawn.",
     respawnTextWithTimer = "You are dead, you can respawn in ~c~%d~w~ seconds.",
-    respawnPosition = vector3(298.29, -584.26, 43.26), -- Change to desired respawn position (Pillbox by default)
-    respawnHeading = 0, -- Change to desired respawn heading (0 by default)
-    bleedoutTime = 120, -- Bleed out time in seconds
+    respawnPosition = vector3(298.29, -584.26, 43.26), -- Change to desired respawn position
+    respawnHeading = 61.98, -- Change to desired respawn heading
     adrevCommand = "command.adrev", -- The Ace permission for /adrev command
-    
-    -- Define medical departments for EMS call
-    MedDept = {
-        "LSPD",
-        "LSFD",
-    },
 }
